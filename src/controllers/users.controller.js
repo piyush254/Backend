cdda1361c3e203fa66fd034668a558092fd22dbd
piyush254 <kumarpiyush155@gmail.com>
2025-08-23 12:12:03 +0530

    making user register

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,9 +1,69 @@
-import {asyncHandler} from "../utils/asyncHandlerHelper.js"
+import { asyncHandler } from "../utils/asyncHandlerHelper.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { uploadCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler(async (req ,resp)=>{
-  resp.status(200).json({
-    message : "ok"
-  })
-})
+const registerUser = asyncHandler(async (req, resp) => {
+  // getting user details from users
+  // Validation - nor empty
+  // Check if user already exists : Via username and email
+  // check for images and check for avtar
+  // Upload them to cloudinary , check for avtar
+  // create User object - Create entry in db
+  // remove passwword and refresh token from response
+  // return response
 
-export {registerUser}
+  const { fullName, email, userName, password } = req.body;
+  console.log("Email ::", email);
+
+  // if(!fullName){
+  //   throw new ApiError(400 , "fullName is required");
+  // }
+
+  if ([fullName, email, userName, password].some(() => field?.trim == "")) {
+    throw new ApiError(
+      400,
+      "All Fields are required check fieldsFullName , Email , userName , password"
+    );
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ userName }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(
+      409,
+      "User already exist with this userName or email try another useName or email"
+    );
+  }
+
+  const AvtarLocalPath = req.files?.avtar[0]?.path;
+  const CoverImageLocalPath = req.files?.CoverImage[0]?.path;
+
+  if (!AvtarLocalPath) throw new ApiError(400, "Avtar file  is required");
+  const avtarPath = await uploadCloudinary(AvtarLocalPath);
+  const coverImagePath = await uploadCloudinary(CoverImageLocalPath);
+  if (!avtarPath) throw new ApiError(400, "Avtar file  is required");
+
+  const user = User.create({
+    fullName,
+    avtar: avtarPath.url,
+    CoverImage: coverImagePath?.url || "",
+    email,
+    password,
+    userName: userName.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) throw new ApiError(500, "Something went wrong");
+
+  return resp
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registed Successfully"));
+});
+
+export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/users.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(upload.fields(
+  {
+    name : "avtar",
+    maxCount : 1
+  },
+  {
+    name : "CoverImage",
+    maxCount: 1
+  }
+) ,registerUser)
 export default router;
